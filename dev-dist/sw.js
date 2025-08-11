4d09fa26a814e77bf919067d3ca149bf6c8a3960
dashapps <121200111+dashapps@users.noreply.github.com>
2025-08-11 04:01:55 -0400

    Update sw.js

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-d9a5ed57'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.v3gfn8tu16g"
+    "revision": "0.c6cpldtnat8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
